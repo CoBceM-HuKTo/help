92db6c137fc414f3fbe5b4c8ef4c7ba9007da2f6
Павел <94619766+CoBceM-HuKTo@users.noreply.github.com>
2023-04-27 12:10:34 +0500

    Update ex2.pas

--- ex2.pas
+++ ex2.pas
@@ -1,29 +1,58 @@
-program Ex1;
-type matrix = array [1..5, 1..5] of integer;
-var a: matrix;
-    i, j, s: integer;
+Program Ochered;
+Uses Crt;
+Const n=1000;
+Var i,j: integer; x,P,Q: real;
+    A,B,C,D,E,F,G: array [1..n] of real;
+    H, L: array [1..11] of real;
 begin
   randomize;
-  for i:=1 to 5 do
+  P:=10;
+  Q:= 5;
+  A[1]:= random*P;
+  B[1]:=random*Q;
+  C[1]:= 0;
+  D[1]:= 0;
+  E[1]:= D[1] + B[1];
+  F[1]:= E[1]-C[1];
+  G[1]:= 0;
+  for i:= 2 to n do
   begin
-    for j:= 1 to 5 do
-    begin
-      a[i, j]:= random(100) - 50;
-    end;
+    A[i]:= random*P;
+    B[i]:=random*Q;
+    C[i]:= C[i-1]+A[i];
+    if c[i]>E[i-1] then
+      D[i]:= C[i]
+    else
+      D[i]:= E[i-1];
+    E[i]:=D[i] + B[i];
+    F[i]:= E[i]-C[i];
+    G[i]:= F[i]-B[i];
   end;
-  for i:=1 to 5 do
-  begin
-    s:= 0;
-    for j:= 1 to 5 do
-    begin
-      if a[i, j] < 0 then
-        s:= s + 1;
-    end;
-    if (s = 2) then
-      begin
-      writeln('Да');
-      exit;
-      end;
-  end;
+  x:= G[i]
+  for i:=2 to n do
+    if G[i]>x then
+      x:= G[i];
+    writeln('Максимальное значение G = ', x);
+    x:= G[1];
+    for i:= 2 to n do
+      x:= x + G[i];
+    x:= x/n;
+    writeln('Среднее значение G = ', x);
+    x:= P;
+    for i:=1 to 11 do
+      L[i]:= (i-1)*x/10;
+    for i:=1 to 11 do
+      H[i]:= 0;
+    for i:= 1 to 10 do
+      for j:=1 to n do
+        if ((G[j]>=L[i]) and (G[j]<L[i+1])) then
+          H[i]:= H[i] + 1;
+    for j:=1 to n do
+      if (G[j]>=P) then
+        H[11]:= H[11] + 1;
+    for i:= 1 to 11 do
+      H[i]:= H[i]/n;
+    for i:=1 to 11 do
+      writeln('G[', i, ']=', H[i]);
+    repeat until keypressed
 end.
-  
